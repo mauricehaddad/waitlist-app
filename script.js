d7b142aab4217f775023a7feae9d1719b66de72a
mauricehaddad <mauricehaddad@gmail.com>
2025-09-22 14:52:02 -0500

    Setup fully working Supabase CDN form

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// No import statement needed when using the CDN
+// Supabase credentials
 const SUPABASE_URL = 'https://vrjaagjafzlkntdrqsuw.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyamFhZ2phZnpsa250ZHJxc3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NjIzMjIsImV4cCI6MjA3NDEzODMyMn0.We8rwTSHSOschZxP1nmu8K34KvWU3UtuFElo99qc4Ag'
 
-// Use the global Supabase object from the CDN
+// Initialize Supabase client (from CDN)
 const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
+// Form elements
 const form = document.getElementById('signup-form')
 const status = document.getElementById('status')
 
+// Handle form submission
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
   const name = form.elements['name'].value
   const email = form.elements['email'].value
 
-  let { data, error } = await supabase
+  const { data, error } = await supabase
     .from('signups')
     .insert([{ name, email }])
 

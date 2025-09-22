7bbcf3e96fb45595b30b39d4b4cae10be40fef71
mauricehaddad <mauricehaddad@gmail.com>
2025-09-22 15:50:37 -0500

    Working local Parcel setup for waitlist form

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Supabase credentials
-
-import { createClient } from '@supabase/supabase-js'
+// Supabase credentials (public anon key for browser)
 const supabaseUrl = 'https://vrjaagjafzlkntdrqsuw.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyamFhZ2phZnpsa250ZHJxc3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NjIzMjIsImV4cCI6MjA3NDEzODMyMn0.We8rwTSHSOschZxP1nmu8K34KvWU3UtuFElo99qc4Ag'
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 document.addEventListener('DOMContentLoaded', () => {
